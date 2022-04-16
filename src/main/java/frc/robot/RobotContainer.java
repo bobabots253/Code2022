4184db1e755aed7a2a5e6d2330248b48733f4eb0
bobabot253 <253programming@gmail.com>
2022-04-15 17:10:43 -0700

    Update Trajectory organization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.time.Instant;
 import java.util.*;
 import java.util.function.BiConsumer;
@@ -26,6 +28,7 @@ import frc.robot.commands.CargoTrack;
 import frc.robot.commands.ConveyorQueue;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.SPI.Port;
@@ -56,6 +59,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -106,8 +110,15 @@ public class RobotContainer {
         drivetrain.resetEncoders();
 
         bindOI();
+        initTrajectories();
     }
     
+    private void initTrajectories() {
+        smallTraj = initializeTrajectory(smallJSON);
+        for(int i = 0; i < autoGroup1.length; i++) {
+        autoGroup1[i] = initializeTrajectory(auto1JSON[i]);
+        }
+    }
 
     private void bindOI() {
         driver_RB.whenHeld(new RunCommand(() -> arm.setOpenLoop(0.05), arm).withTimeout(1.7))
@@ -235,6 +246,25 @@ public class RobotContainer {
         return instance;
     }
 
+    
+    public static Trajectory smallTraj = new Trajectory();
+    private static final String smallJSON = "paths/Small.wpilib.json";
+    private static final String[] auto1JSON = {"paths/Auto1.wpilib.json", "paths/Auto2.wpilib.json", "paths/Auto3.wpilib.json"};
+    public static Trajectory[] autoGroup1 = new Trajectory[3];
+
+    public static Trajectory initializeTrajectory(final String tjson) {
+        Trajectory t = null;
+        Path tPath = Filesystem.getDeployDirectory().toPath().resolve(tjson);
+        try {
+          t = TrajectoryUtil.fromPathweaverJson(tPath);
+        } catch (IOException e) {
+          System.out.println("silly pathweaver bad");
+          e.printStackTrace();
+          DriverStation.reportError("PATH FAILED", e.getStackTrace());
+        }
+        return t;
+      }
+
     /*
         @param trajectory: the trajectory to follow
     */
@@ -275,7 +305,7 @@ public class RobotContainer {
         Drivetrain.ODOMETRY.resetPosition(trajectory.getInitialPose(), navX.getRotation2d());
 
         // Run path following command, then stop at the end.
-        return ramseteCommand.andThen(() -> Drivetrain.setOpenLoop(0, 0));
+        return new InstantCommand(() -> Drivetrain.ODOMETRY.resetPosition(trajectory.getInitialPose(), navX.getRotation2d())).andThen(ramseteCommand.andThen(() -> Drivetrain.setOpenLoop(0, 0)));
     }
 
      /**
