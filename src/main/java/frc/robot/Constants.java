294fb934c446f2dcf34b5a1d03909af8c76a6f7b
bobabot253 <253programming@gmail.com>
2022-04-18 15:00:36 -0700

    Update vision constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
 
     public static class DriverConstants {
         /* Common drive mode settings */
-        public static final double kJoystickDeadband = 0.05; // How much of joystick is "dead" zone [0,1]
+        public static final double kJoystickDeadband = 0.07; // How much of joystick is "dead" zone [0,1]
         public static final double kDriveSens = 1.0; // Overall speed setting (turn down for demos) [0,1]
         public static final double kTurnInPlaceSens = 0.3; // Maximum turn-in-place rate (in percent of max) to allow
                                                             // robot to turn to [0,1]
@@ -142,8 +142,8 @@ public class Constants {
         /* Turn PID Constants */
         public static double kPTurn = 0.1;
         public static double kITurn = 0;
-        public static double kDTurn = 0; //TODO: tune D
-        public static double kTurnTolerance = 0;
+        public static double kDTurn = 0.003; //TODO: tune D
+        public static double kTurnTolerance = 0.0;//1.07;
 
         /* Distance PID Constants */
         public static double kPDist = 0.1;
